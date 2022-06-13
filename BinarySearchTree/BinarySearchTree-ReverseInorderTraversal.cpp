dd68a2af672d6d795fd5cd7647a28ce2d2584ab5
Akshat Singh <51196518+akshatrksingh@users.noreply.github.com>
2022-06-13 23:43:28 +0530

    Add files via upload

---
+++ BinarySearchTree/BinarySearchTree-ReverseInorderTraversal.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+	int data;
+	node* left;
+	node* right;
+
+	node(int d)
+	{
+		data = d;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+node* insert(node *root, int data)
+{
+	if(root == NULL)
+	{
+		return new node(data);
+	}
+	if(data <= root->data)
+	{
+		root->left = insert(root->left, data);	
+	}
+	else
+	{
+		root->right = insert(root->right, data);
+	}
+	return root; 
+}
+
+node* buildTree()
+{
+	int d;
+	cout<<"";
+	cin>>d;
+
+	node* root = NULL;
+	while(d != -1)
+	{
+		root = insert(root, d);
+		cin>>d;
+	}
+	return root;
+
+}
+
+void display(node *root)
+{
+	cout<<"";
+	if(root == NULL)
+	{
+		return;
+	}	
+
+	display(root->right);
+	cout<<root->data<<" ";
+	display(root->left);
+}
+
+int main()
+{
+	node* root = buildTree();
+	display(root);
+	return 0;
+}
